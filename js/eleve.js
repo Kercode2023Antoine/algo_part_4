93829d59fb0cefaf9d1b5968bfb4ce9fc5ea29fe
Antoine Boeraeve <antoine.boeraeve@gmail.com>
2023-11-20 12:08:33 +0100

    création

---
+++ js/eleve.js
@@ -0,0 +1,14 @@
+function Eleve(n, p, c ,no){
+
+    Eleve.nom = n;
+    Eleve.prenom =p;
+    Eleve.classe = c;
+    Eleve.notes = {
+        maths : [],
+        algorithmique : [],
+        javaScript :[],
+    } = no;
+
+    }
+
+    
